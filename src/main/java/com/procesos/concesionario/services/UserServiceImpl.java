45a99974af3e17b7e56fce4f9d52ad0a862321c3
WilderDuarte <waduarten@ufpso.edu.co>
2023-03-16 20:45:23 -0500

    Creacion de servicios y endopoint de crear un usuario

---
+++ src/main/java/com/procesos/concesionario/services/UserServiceImpl.java
@@ -0,0 +1,40 @@
+package com.procesos.concesionario.services;
+
+import com.procesos.concesionario.models.User;
+import com.procesos.concesionario.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User getUserById(Long id){
+        return userRepository.findById(id).get();
+    }
+
+    @Override
+    public List<User> allUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User updateUser(Long id, User user) {
+        User userBD = userRepository.findById(id).get();
+        userBD.setFirstName(user.getFirstName());
+        userBD.setLastName(user.getLastName());
+        userBD.setAddress(user.getAddress());
+        userBD.setBirthday(user.getBirthday());
+        return userRepository.save(userBD);
+    }
+}
